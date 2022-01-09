58c39c1bf7e0fded2f98e624fc7a689bf5e7bde3
rohit rai <rohitrai.dev@gmail.com>
2022-01-09 16:15:57 +0530

    connected to alan backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
+import alanBtn from '@alan-ai/alan-sdk-web';
+const alanKey='7887c008b36d84adaecc1f8666573fbe2e956eca572e1d8b807a3e2338fdd0dc/stage'
 function App() {
+  useEffect(() => {
+    alanBtn({
+      key:alanKey,
+      onCommand:({command})=>{
+        if(command==='testCommand'){
+          alert("hi")
+        }
+      }
+    })
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     alan
     </div>
   );
 }
